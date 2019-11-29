ad81fae5392b22fcd17d9a707a367852f1a25c6b
Brad Alexander <brad.alexander913@gmail.com>
2019-11-29 22:17:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team = Team.new(params[:team][:name], params[:team][:motto])
-      params[heroes].each do |h|
+      params[:heroes].each do |h|
         Hero.new(h)
       end
 
